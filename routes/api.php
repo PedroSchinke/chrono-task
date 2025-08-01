a8cbc7316cca1358b3a67571568634a61a5f27eb
Pedro Schinke <pedro.schinke@outlook.com>
2025-07-31 22:26:43 -0300

    criando rotas colaboradores

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ColaboradoresController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MaquinaController;
 use App\Http\Controllers\TarefaController;
@@ -18,6 +19,12 @@ Route::get('/tarefas', [TarefaController::class, 'index']);
 Route::post('/tarefa', [TarefaController::class, 'store']);
 Route::post('/tarefa/{tarefa_id}', [TarefaController::class, 'update']);
 Route::post('/tarefa/{tarefa_id}/reposicionar', [TarefaController::class, 'reposicionarTarefa']);
+
+/**
+ * Colaboradores
+ */
+Route::get('/colaboradores', [ColaboradoresController::class, 'index']);
+Route::post('/colaboradores', [ColaboradoresController::class, 'store']);
 
 /**
  * Horários Disponíveis
