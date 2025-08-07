da8ba5882a2f5a3dd4250babf1f72490c07539b0
Pedro Schinke <pedro.schinke@outlook.com>
2025-08-06 23:44:12 -0300

    corrigidos relacionamentos

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -8,22 +8,16 @@ class Tarefa extends Model
 {
     protected $table = 'tarefas';
     protected $primaryKey = 'id';
-    protected $fillable = [
-        'titulo',
-        'descricao',
-        'inicio',
-        'fim',
-        'cor',
-    ];
+    protected $fillable = ['titulo', 'descricao', 'inicio', 'fim', 'cor'];
     public $timestamps = false;
 
     public function colaboradores()
     {
-        return $this->belongsTo(Colaborador::class);
+        return $this->belongsToMany(Colaborador::class, 'colaborador_tarefa', 'tarefa_id', 'colaborador_id');
     }
 
     public function maquinas()
     {
-        return $this->belongsTo(Maquina::class);
+        return $this->belongsToMany(Maquina::class, 'maquina_tarefa', 'tarefa_id', 'maquina_id');
     }
 }
