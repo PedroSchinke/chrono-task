99097f59d15b33e7f122e238e63a2045cebbdd01
Pedro Schinke <pedro.schinke@outlook.com>
2025-08-14 22:33:34 -0300

    criado e implementado em rotas middleware de transaction

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\TransactionMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(TransactionMiddleware::class);
+
+        $middleware->alias([
+            'transaction' => TransactionMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,13 @@ use App\Http\Controllers\TarefaController;
 /**
  * Máquinas
  */
-Route::get('/maquinas', [MaquinaController::class, 'index']);
+Route::get('/maquinas', [MaquinaController::class, 'index'])->withoutMiddleware(['transaction']);
 Route::post('/maquina', [MaquinaController::class, 'store']);
 
 /**
  * Tarefas
  */
-Route::get('/tarefas', [TarefaController::class, 'index']);
+Route::get('/tarefas', [TarefaController::class, 'index'])->withoutMiddleware(['transaction']);
 Route::post('/tarefa', [TarefaController::class, 'store']);
 Route::post('/tarefa/{tarefa_id}', [TarefaController::class, 'update']);
 Route::post('/tarefa/deletar/{tarefa_id}', [TarefaController::class, 'destroy']);
@@ -23,5 +23,5 @@ Route::post('/tarefa/{tarefa_id}/reposicionar', [TarefaController::class, 'repos
 /**
  * Colaboradores
  */
-Route::get('/colaboradores', [ColaboradorController::class, 'index']);
+Route::get('/colaboradores', [ColaboradorController::class, 'index'])->withoutMiddleware(['transaction']);
 Route::post('/colaboradores', [ColaboradorController::class, 'store']);
